601050965c3525e1fda3ffec29e900d6fc660f5d
Avenardin <alex18g76@mail.ru>
2019-03-30 23:07:55 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -2,5 +2,18 @@ public class Calculate{
  
 	public static void main(String[] arg){
 		System.out.println("Calculate...");
+		double first = Integer.valueOf(arg[0]);
+		double second = Integer.valueOf(arg[1]);
+		double summ = first + second;
+		double mult = first * second;
+		double div = first / second;
+		
+		System.out.println("Sum " + summ);
+		System.out.println("mult " + mult);
+		System.out.println("div " + div);
+	
 	}
 };
+
+  
+
